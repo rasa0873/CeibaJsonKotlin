13d552813f11b7d916457e36e0038f83aa520c2d
rasa0873 <raulsiraalvarez@gmail.com>
2022-05-06 17:56:37 -0400

    Update 3

--- app/src/main/java/com/rasaapps/raul/ceibajsonkotlin/MainActivity.kt
+++ app/src/main/java/com/rasaapps/raul/ceibajsonkotlin/MainActivity.kt
@@ -2,7 +2,6 @@ package com.rasaapps.raul.ceibajsonkotlin
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.Menu
 import android.view.View
 import android.widget.ProgressBar
@@ -15,6 +14,8 @@ import com.android.volley.Request
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONException
+import java.io.*
+
 
 class MainActivity : AppCompatActivity(), JumpListener {
 
@@ -30,6 +31,14 @@ class MainActivity : AppCompatActivity(), JumpListener {
     var userPhones: List<String>? = null
     var userMails: List<String>? = null
     var userPos: List<String>?= null
+
+    // Internal files for saving user data
+    var fileWriteStream: FileOutputStream? = null
+    var fileReadStream: FileInputStream? = null
+    private val USER_NAME_FILENAME = "user_name_ceiba.txt"
+    private val USER_PHONE_FILENAME = "user_phone_ceiba.txt"
+    private val USER_EMAIL_FILENAME = "user_email_ceiba.txt"
+    private val USER_POS_FILENAME = "user_pos_ceiba.txt"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,6 +70,13 @@ class MainActivity : AppCompatActivity(), JumpListener {
             (userMails as ArrayList<String>).add(userModalArrayList!![i].getUserEmail()!!)
             (userPos as ArrayList<String>).add(userModalArrayList!![i].getUserPosition()!!)
         }
+
+        updateInternalFiles (userNames as ArrayList<String>,
+            userPhones as ArrayList<String>,
+            userMails as ArrayList<String>,
+            userPos as ArrayList<String>
+        )
+
     }
 
     private fun getData() {
@@ -88,9 +104,14 @@ class MainActivity : AppCompatActivity(), JumpListener {
                 }
                 copyArrayList()
             }
-        ) { Toast.makeText(this, "Fail to get the data..", Toast.LENGTH_SHORT).show() }
+        ) {
+            Toast.makeText(this, "Fail to get the data..", Toast.LENGTH_SHORT).show()
+            // Retrieving data fom internal files
+            restoreFromFiles()
+        }
         queue.add(jsonArrayRequest)
     }
+
 
     fun buildRecyclerView() {
         // initializing our adapter class.
@@ -145,6 +166,94 @@ class MainActivity : AppCompatActivity(), JumpListener {
         i.putExtra("id", id)
         startActivity(i)
    }
+
+    private fun updateInternalFiles(name: List<String>, phone: List<String>,
+                                    email: List<String>, pos: List<String>) {
+
+        try { // Names List internal file
+            fileWriteStream = openFileOutput(USER_NAME_FILENAME, MODE_PRIVATE)
+            for (i in name.indices) {
+                with(fileWriteStream) { this?.write((name[i]+"888").toByteArray()) }
+            }
+            with(fileWriteStream) { this?.close() }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        try { // Phone List internal file
+            fileWriteStream = openFileOutput(USER_PHONE_FILENAME, MODE_PRIVATE)
+            for (i in phone.indices) {
+                with(fileWriteStream) { this?.write((phone[i]+"\n").toByteArray()) }
+            }
+            with(fileWriteStream) { this?.close() }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        try {  // Email List internal file
+            fileWriteStream = openFileOutput(USER_EMAIL_FILENAME, MODE_PRIVATE)
+            for (i in email.indices) {
+                with(fileWriteStream) { this?.write((email[i]+"\n").toByteArray()) }
+            }
+            with(fileWriteStream) { this?.close() }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        try {  // Email List internal file
+            fileWriteStream = openFileOutput(USER_POS_FILENAME, MODE_PRIVATE)
+            for (i in pos.indices) {
+                with(fileWriteStream) { this?.write((pos[i]+"\n").toByteArray()) }
+            }
+            with(fileWriteStream) { this?.close() }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun readInternalFile(fileName : String) {
+
+        try {
+            fileReadStream = openFileInput(fileName)
+            val inputStreamReader : InputStreamReader = InputStreamReader(fileReadStream)
+            val bufferedReader: BufferedReader = BufferedReader(inputStreamReader)
+            val stringBuilder : StringBuilder = StringBuilder()
+            var text : String? = null
+            while ({text = bufferedReader.readLine(); text} ()!=null){
+                stringBuilder.append(text)
+            }
+            val details = stringBuilder.split("888") // details is a List<String>
+            if (fileName.equals(USER_NAME_FILENAME))
+                userNames = details.dropLast(1) // userNames fully retrieved
+            else if (fileName.equals(USER_PHONE_FILENAME))
+                userPhones = details.dropLast(1) // userPhones fully retrieved
+            else if (fileName.equals(USER_EMAIL_FILENAME))
+                userMails = details.dropLast(1) // userMails fully retrieved
+            else
+                userPos = details.dropLast(1) // userPos fully retrieved
+        } catch (e: FileNotFoundException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
+    }
+
+
+    fun restoreFromFiles() {
+
+        readInternalFile(USER_NAME_FILENAME)
+        readInternalFile(USER_PHONE_FILENAME)
+        readInternalFile(USER_EMAIL_FILENAME)
+        readInternalFile(USER_POS_FILENAME)
+
+        userModalArrayList!!.clear() // clear the list first
+        // Now reconstruct the array list
+        for (x in userNames!!.indices) {
+            userModalArrayList!!.add(UserModal(
+                userNames!![x], userMails!![x],
+                userPhones!![x], userPos!![x]))
+        }
+        buildRecyclerView() // refresh the RV
+    }
+
 
 
 }
